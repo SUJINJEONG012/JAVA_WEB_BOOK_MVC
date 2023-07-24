5b3863e2f6adb47f18f78f8d87d1f755bdf9ef8c
SUJINJEONG012 <peekaboo32@naver.com>
2023-07-24 20:10:08 +0900

    자바웹개발워크북책 참고하여 공부

---
+++ src/main/java/com/angela/JavaWebBookMvcApplication.java
@@ -0,0 +1,13 @@
+package com.angela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebBookMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebBookMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angela/JavaWebBookMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebBookMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
