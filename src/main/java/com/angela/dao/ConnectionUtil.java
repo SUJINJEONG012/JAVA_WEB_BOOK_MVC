6a8f58b761a65781ab6b271b51997e0352eacdd3
SUJINJEONG012 <peekaboo32@naver.com>
2023-07-26 23:03:51 +0900

    enum 으로 ConnectionUtil 생성

---
+++ src/main/java/com/angela/dao/ConnectionUtil.java
@@ -0,0 +1,30 @@
+package com.angela.dao;
+
+import java.sql.Connection;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public enum ConnectionUtil {
+	
+	INSTANCE;
+	
+	private HikariDataSource ds;
+	
+	ConnectionUtil(){
+		HikariConfig config = new HikariConfig();
+		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		config.setJdbcUrl("jdbc:mysql://localhost:3306/webdb");
+		config.setUsername("root");
+		config.setPassword("1234");
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        ds = new HikariDataSource(config);
+	}
+	public Connection getConnection() throws Exception{
+		return ds.getConnection();
+	}
+	
+}
