4f8675f2952de953ea8f7008189eac1c8dba83e3
angela <peekaboo32@naver.com>
2023-07-26 16:40:45 +0900

    Connection Pool TEST

--- src/test/java/com/angela/dao/ConnectTests.java
+++ src/test/java/com/angela/dao/ConnectTests.java
@@ -1,7 +1,9 @@
 package com.angela.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -17,19 +19,19 @@ public class ConnectTests {
 //		Assertions.assertEquals(v1, v2);
 //	}
 
-//	@Test
-//	public void testConnection() throws Exception {
-//		Class.forName("com.mysql.cj.jdbc.Driver");
-//
-//		Connection connection = DriverManager.getConnection(
-//				"jdbc:mysql://localhost:3306/webdb", "root", "1234");
-//
-//		Assertions.assertNotNull(connection);
-//		
-//		System.out.println("connection: " + connection);
-//		//반드시 connection은 close() 해야함.
-//		connection.close();
-//	}
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/webdb", "root", "1234");
+
+		Assertions.assertNotNull(connection);
+		
+		System.out.println("connection: " + connection);
+		//반드시 connection은 close() 해야함.
+		connection.close();
+	}
 	
 	
 	@Test
@@ -41,15 +43,15 @@ public class ConnectTests {
 		config.setUsername("root");
 		config.setPassword("1234");
 		config.addDataSourceProperty("cachePrepStmts", "true");
-		config.addDataSourceProperty("prepStmtCacheSize", "250");
-		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
 		
 		HikariDataSource ds = new HikariDataSource(config);
 		Connection connection = ds.getConnection();
 		
 		System.out.println(connection);
 		connection.close();
-		
 		
 	}
 }
