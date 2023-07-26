36a0db7f74b6fc82bd485fe61f5097b7fca476be
angela <peekaboo32@naver.com>
2023-07-26 14:24:36 +0900

    HikariCP를 이용하기 위해  HikariConfig 타입의 객체 생성
    HikariConfig은 connection Pool을 설정하는 필요한 정보를 가지고 있는 객체로 이를 이용해서 HikariDataSource라는 객체를 생성

--- src/test/java/com/angela/dao/ConnectTests.java
+++ src/test/java/com/angela/dao/ConnectTests.java
@@ -1,34 +1,55 @@
 package com.angela.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectTests {
 
+//	@Test
+//	public void test1() {
+//		int v1 = 10;
+//		int v2 = 10;
+//		
+//		Assertions.assertEquals(v1, v2);
+//	}
+
+//	@Test
+//	public void testConnection() throws Exception {
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//		Connection connection = DriverManager.getConnection(
+//				"jdbc:mysql://localhost:3306/webdb", "root", "1234");
+//
+//		Assertions.assertNotNull(connection);
+//		
+//		System.out.println("connection: " + connection);
+//		//반드시 connection은 close() 해야함.
+//		connection.close();
+//	}
+	
+	
 	@Test
-	public void test1() {
-		int v1 = 10;
-		int v2 = 10;
+	public void testHikariCP() throws Exception{
 		
-		Assertions.assertEquals(v1, v2);
-
-	}
-
-	@Test
-	public void testConnection() throws Exception {
-
-		Class.forName("com.mysql.cj.jdbc.Driver");
-
-		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/webdb", "root", "1234");
-
-		Assertions.assertNotNull(connection);
+		HikariConfig config =  new HikariConfig();
+		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		config.setJdbcUrl("jdbc:mysql://localhost:3306/webdb");
+		config.setUsername("root");
+		config.setPassword("1234");
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
 		
-		System.out.println("connection: " + connection);
-		//반드시 connection은 close() 해야함.
+		HikariDataSource ds = new HikariDataSource(config);
+		Connection connection = ds.getConnection();
+		
+		System.out.println(connection);
 		connection.close();
+		
+		
 	}
 }
