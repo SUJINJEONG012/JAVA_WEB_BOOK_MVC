f9e2ef06d38304ed6b9de9daa2812c1bec6b1618
angela <peekaboo32@naver.com>
2023-07-27 10:19:10 +0900

    getTime()테스트 실행 완료

---
+++ src/test/java/com/angela/dao/TodoDaoTests.java
@@ -0,0 +1,18 @@
+package com.angela.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TodoDaoTests {
+	private TodoDao todoDao;
+	
+	@BeforeEach
+	public void ready() {
+		todoDao = new TodoDao();
+	}
+	
+	@Test
+	public void testTime() throws Exception{
+		System.out.println(todoDao.getTime());
+	}
+}
