b18c6dd78ec8a15afa16e5a0232f92a18e0361e9
angela <peekaboo32@naver.com>
2023-07-27 11:17:41 +0900

    insert 테스트 성공, 데이터베이스에 추가 확인

--- src/test/java/com/angela/dao/TodoDaoTests.java
+++ src/test/java/com/angela/dao/TodoDaoTests.java
@@ -1,7 +1,11 @@
 package com.angela.dao;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.angela.vo.TodoVo;
 
 public class TodoDaoTests {
 	private TodoDao todoDao;
@@ -14,6 +18,16 @@ public class TodoDaoTests {
 	@Test
 	public void testTime() throws Exception{
 		System.out.println(todoDao.getTime());
+	}
+	
+	@Test
+	public void testInsert() throws Exception {
+		TodoVo todoVo = TodoVo.builder()
+				.title("sample Title...")
+				.dueDate(LocalDate.of(2023, 7, 27))
+				.build();
+		todoDao.insert(todoVo);
+		
 	}
 	
 
