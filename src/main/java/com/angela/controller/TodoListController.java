62454f3a39d5169b288fae3fa6179e5abc477b3e
angela <peekaboo32@naver.com>
2023-07-28 17:48:42 +0900

    목록 Controller 구현중....

--- src/main/java/com/angela/controller/TodoListController.java
+++ src/main/java/com/angela/controller/TodoListController.java
@@ -1,19 +1,24 @@
 package com.angela.controller;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import com.angela.dto.TodoDto;
 import com.angela.service.TodoService;
 
+import lombok.extern.log4j.Log4j2;
+
 @WebServlet(name="todoController", urlPatterns="/todo/list")
+@Log4j2
 public class TodoListController extends HttpServlet{
 
+	private TodoService todoService = TodoService.INSTACNE;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws Exception, IOException {
+		log.info("todo list....................");
+	}
 
 }
