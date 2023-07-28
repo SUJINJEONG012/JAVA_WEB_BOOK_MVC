02ca4de4566944a140743084cae584efc1345a10
angela <peekaboo32@naver.com>
2023-07-28 11:30:53 +0900

    dto에 롬복관련 어노테이션 추가

--- src/main/java/com/angela/dto/TodoDto.java
+++ src/main/java/com/angela/dto/TodoDto.java
@@ -2,59 +2,18 @@ package com.angela.dto;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoDto {
 	private Long tno;
 	private String title;
 	private LocalDate dueDate;
-	public LocalDate getDueDate() {
-		return dueDate;
-	}
-
-
-	public void setDueDate(LocalDate dueDate) {
-		this.dueDate = dueDate;
-	}
-
-
 	private boolean finished;
-	
-	
-	public Long getTno() {
-		return tno;
-	}
-
-
-	public void setTno(Long tno) {
-		this.tno = tno;
-	}
-
-
-	public String getTitle() {
-		return title;
-	}
-
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-
-	public boolean isFinished() {
-		return finished;
-	}
-
-
-	public void setFinished(boolean finished) {
-		this.finished = finished;
-	}
-
-
-	@Override
-	public String toString() {
-		return "TodoDto [tno=" + tno + ", title=" + title + ", dueDate=" + dueDate + ", finished=" + finished + "]";
-	}
-	
 }
